916b3aea48824e8664c2c1b6ca7b53316addbed6
Fabio <fabioescobarardila@hotmail.com>
2024-09-01 12:08:20 -0500

    Ejemplo de Clase Abstracta, mini proyecto abstractClass_geometrics

---
+++ Mini_Proyectos_Teoria/abstractClass_geometrics/src/Square.java
@@ -0,0 +1,13 @@
+public class Square extends GeometricFigure{
+    private Double sideLength;
+
+    public Square( Double sideLength){
+        super(4);
+        this.sideLength=sideLength;
+    }
+
+    @Override
+    public Double calculateArea() {
+        return sideLength * sideLength;
+    }
+}
